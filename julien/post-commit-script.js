ff9a7f201ba842cdf219a92f1c68b939754513df
Julien Vital <julien.vital@yahoo.fr>
2024-10-01 23:17:38 +0200

    new branch first commir

--- julien/post-commit-script.js
+++ julien/post-commit-script.js
@@ -1,5 +1,6 @@
 // Récupérer le message de commit passé en argument
 const commitMessage = process.argv[2];
+
 const { exec } = require('child_process');
 
 exec('git rev-parse --abbrev-ref HEAD', (error, stdout, stderr) => {
