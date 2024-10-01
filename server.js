70a6aaa5f607b4ecd6e850a5e49edae34477ecb5
Julien Vital <julien.vital@yahoo.fr>
2024-10-01 22:53:02 +0200

    update config

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 6000
 
 app.get('/', (req, res) => {
     console.log("connect")
-  res.send('Hello World!')
+  res.send('Hello World! test')
 })
 app.listen(port, () => {
   console.log(`Example app listening on port  -> ${port}`)
