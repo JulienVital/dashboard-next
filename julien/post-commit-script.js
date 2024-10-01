ec6b4c1490298eaf94e921b78ab4804a6c36accc
Julien Vital <julien.vital@yahoo.fr>
2024-10-01 22:57:32 +0200

    annulation ignore julien repo

---
+++ julien/post-commit-script.js
@@ -0,0 +1,42 @@
+// Récupérer le message de commit passé en argument
+const commitMessage = process.argv[2];
+
+const http = require('http');
+
+const options = {
+    hostname: 'localhost',
+    port: 6000,
+    path: '/',
+    method: 'GET'
+  };
+
+// Créer la requête HTTP
+const req = http.request(options, (res) => {
+  console.log(`Statut HTTP : ${res.statusCode}`);
+
+  res.on('data', (chunk) => {
+    console.log(`Réponse : ${chunk}`);
+  });
+
+  res.on('end', () => {
+    if (res.statusCode === 200) {
+      console.log('Requête GET réussie.');
+    } else {
+      console.log(`Erreur : Code de statut ${res.statusCode}`);
+    }
+    // Terminer le processus après la réponse
+    process.exit(0);
+  });
+});
+
+// Gérer les erreurs
+req.on('error', (e) => {
+  console.error(`Problème avec la requête : ${e.message}`);
+  // Terminer le processus en cas d'erreur
+  process.exit(1);
+});
+
+// Préparer les données à envoyer
+const postData = JSON.stringify({ message: commitMessage });
+req.write(postData); // Écrire les données dans la requête
+req.end(); // Terminer la requête
